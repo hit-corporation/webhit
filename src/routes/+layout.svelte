1c3cb9cd8bdc7b5fb6e386326f846b235c54bb7b
nengmoemoen <nengmoemoen@gmail.com>
2026-02-22 05:25:35 +0700

    Add Layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
+  	import Header from '$lib/components/layouts/Header.svelte';
+	import Footer from '$lib/components/layouts/Footer.svelte';
 	import '../app.css';
 	let { children } = $props();
 </script>
 
+<Header />
 {@render children()}
+<Footer />
